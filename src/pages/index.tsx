1b3c5e85e03b46113e1e041d1f6cc81a9401bdae
shringo <143767857+shringo@users.noreply.github.com>
2023-12-05 10:02:04 -1000

    try and call api inside app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const hello = api.item.getAll.useQuery();
 
   return (
     <>
@@ -43,7 +43,7 @@ export default function Home() {
             </Link>
           </div>
           <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            {JSON.stringify(hello.data, null, '\t')}
           </p>
         </div>
       </main>
