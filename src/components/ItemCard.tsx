d2d364e7c039ca3dcf407583421bffe097e55166
unknown <cibanez11173@gmail.com>
2024-01-08 14:34:26 -1000

    1/8 attempted making reserving item counter

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,9 +1,16 @@
+import { useCallback, useState } from "react";
 export default function ItemCard() {
+    //Temporary place holders 
     const itemName = "Apple"
     const itemPrice = "4.00"
     const itemImage = "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
     const itemStock = 4
     const itemRestockDate = "10/12/23"
+
+    //Counter for number reserving button
+    const [reserving, setreserving] = useState(0);
+    const increaseReserving = useCallback(() => setreserving(state => state+1), setreserving)
+    const decreaseReserving = useCallback(() => setreserving(state => state-1), setreserving)
     return (
         <>
             <div className="card w-96 bg-base-100 shadow-xl">
@@ -35,7 +42,19 @@ export default function ItemCard() {
                         </div>
                     }
                     {/* Reserve button */}
-                    <button className="btn btn-primary">Reserve</button>
+                    <div className="join join-horizontal bg-blue-700">
+                        
+                        
+<button onClick={decreaseReserving}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+</svg>
+ </button>
+                            <p className="items-center">Reserving: {reserving} {itemName}</p>
+                            <button onClick={increaseReserving}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+</button>
+                    </div>
                 </div>
 
             </div>
