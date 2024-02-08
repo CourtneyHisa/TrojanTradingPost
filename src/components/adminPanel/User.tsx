095726d47c0c18e77bf7574f2bcfdd24e42ae358
kaizukaaizumi <111713120+kaizukaaizumi@users.noreply.github.com>
2024-02-08 10:49:46 -1000

    users

--- src/components/adminPanel/User.tsx
+++ src/components/adminPanel/User.tsx
@@ -1,10 +1,39 @@
 export default function Users() {
     return (
         <>
-            <div>
-                <h2>
-                    Users
-                </h2>
+            <div className="overflow-x-auto">
+                <table className="table table-xs">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>E-Mail</th>
+                            <th>Password</th>
+                            <th>Reserves</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>${'user.id'}</th>
+                            <td>${'user.name'}</td>
+                            <td>${'user.email'}</td>
+                            <td>${'user.password'}</td>
+                            <td>
+                                <ul className="menu lg:menu-horizontal bg-base-200 rounded-box lg:mb-64">
+                                    <li>
+                                        <details>
+                                            <summary>View Reserves</summary>
+                                            <ul>
+                                                <li><a>Reserve 1</a></li>
+                                                <li><a>Reserve 2</a></li>
+                                            </ul>
+                                        </details>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     )
--- src/pages/adminpage.tsx
+++ src/pages/adminpage.tsx
@@ -19,9 +19,9 @@ export default function AdminPage() {
             const root = createRoot(main)
             root.render(
                 selectedMenuItem === 'home' ? <Home /> :
-                selectedMenuItem === 'products' ? <Products /> :
-                selectedMenuItem === 'users' ? <Users /> :
-                null
+                    selectedMenuItem === 'products' ? <Products /> :
+                        selectedMenuItem === 'users' ? <Users /> :
+                            null
             );
         } else {
             main?.innerHTML == ''; // Clear the main div if no menu item is selected
@@ -29,12 +29,20 @@ export default function AdminPage() {
     }, [selectedMenuItem]);
 
     return (
+
         <>
+            {/* <label className="btn btn-circle swap swap-rotate">
+
+                <input type="checkbox" />
+                <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+                <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
+
+            </label> */}
             <div>
                 <h2>Admin Page</h2>
             </div>
             <div id="main" ref={mainRef}>
-                <Home/>
+                <Home />
             </div>
             <div className="drawer">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle" />
