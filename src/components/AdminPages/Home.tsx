1a6f4922a7ff015b552db2f1dbf9d8d570d7df54
kaizukaaizumi <111713120+kaizukaaizumi@users.noreply.github.com>
2024-04-01 14:37:34 -1000

    home start

--- src/components/AdminPages/Home.tsx
+++ src/components/AdminPages/Home.tsx
@@ -1,30 +1,44 @@
+import Footer from "../Footer";
+import ItemCard from "../ItemCard";
+import Navbar from "../Navbar";
+import PictureRotation from "../pictureRotation";
+
 export default function Home() {
+
+    const information = {
+        about: "The Trojan Trading Post, or TTP for short, is a student-led pop-up shop that features different products created by students within the various Career and Technical Education pathways. We aim to foster Mililani school spirit and pride",
+        location: "95-1248 Meheula Pkwy, Mililani Town, Hawaii",
+        shopTime: {date: ["November 17", "December 16"], dayTime: { day: ["Friday", "Saturday"], time: ["3PM", "8PM"] }}
+    }
+
     return (
         <>
-            <div>
-                <h2>Home</h2>
-            </div>
-            <div className="overflow-x-auto">
-                <table className="table table-xs">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Item Request</th>
-                            <th>Contact</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>${'reservation.id'}</th>
-                            <td>${'reservation.item'}</td>
-                            <td>${'reservation.contact'}</td>
-                            <td>
-                                <button className="btn btn-sm h-2">Cancel</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <Navbar />
+            <div className="bg-cream1 font-mhs">
+                <PictureRotation />
+                <div className="text-2xl md:px-16 xl:px-24 xl:text-3xl pb-8">
+                    <div className="grid grid-cols-2 md:mt-8 xl:mt-16">
+                        <div className="md:mr-4 xl:mr-20 md:p-6 border-8 border-brown2 bg-tan2 text-beige1 w-fill rounded-lg">
+                            {/* Trojan Trading Post About */}
+                            <h2 className="font-bold underline text-2xl xl:text-4xl pb-2">What is the Trojan Trading Post?</h2>
+                            <p className="">{information.about}</p>
+                        </div>
+                        <div className="md:ml-4 xl:ml-20 md:p-6 border-8 border-brown2 bg-tan2 text-beige1 w-fill rounded-lg">
+                            <div>
+                                {/* Location */}
+                                <h2 className="font-bold underline text-2xl xl:text-4xl pb-2">Location</h2>
+                                <p className="pb-4">{information.location}</p>
+                                {/* Opening Duration */}
+                                <h2 className="font-bold underline text-2xl xl:text-4xl pt-2 pb-2">Open from</h2>
+                                <p className="pb-2">
+                                    {information.shopTime.date[1]} - {information.shopTime.date[2]} <br />
+                                    Friday:3PM-8PM <br />
+                                    Saturday:10AM-8PM
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 import { Items } from "~/utils/loyverse";
 export default function ItemCard({ items }: { items: Items }) {
 
-  let itemList = [
+  const itemList = [
     { itemName: "Apple", itemPrice: "4.00", itemDescription: "Fresh, red, grown locally by agriculture classes", itemImage: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg", itemStock: 6, itemRestockDate: "10/12/23" },
     { itemName: "Lemon", itemPrice: "4.00", itemDescription: "Fresh, grown locally by agriculture classes, good in lemonade", itemImage: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg", itemStock: 6, itemRestockDate: "10/12/23" },
     { itemName: "Dog", itemPrice: "10.00", itemDescription: "Fresh, edible and mustache", itemImage: "https://i.redd.it/1eog44lx72681.jpg", itemStock: 6, itemRestockDate: "10/12/23" },
