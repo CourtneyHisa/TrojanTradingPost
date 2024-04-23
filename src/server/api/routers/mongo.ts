e348ca587e95379a1ec89d39bb8284e9b1e67594
NCarlisle24 <lorenzonolan12983@gmail.com>
2024-04-22 14:37:11 -1000

    created centralized variant/item call

--- src/server/api/routers/mongo.ts
+++ src/server/api/routers/mongo.ts
@@ -2,26 +2,41 @@ import { z } from "zod";
 import axios from "axios";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { API_BASE, AUTH, LVVariantsWrapper } from "~/utils/loyverse";
+import { API_BASE, AUTH, type LVItemsWrapper, type Item, type Inventory, Variant, VariantLinkedItem, VaritStock, LVCategoriesWrapper } from "~/utils/loyverse";
 import { Prisma } from "@prisma/client";
 import { db } from "~/server/db";
 
 export const mongoRouter = createTRPCRouter({
-  getItemByLoyverseId: publicProcedure
+  getItemByLoyverseVariantId: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
-      return await ctx.db.item.findFirst({
+      let mongoData = await ctx.db.item.findFirst({
         where: {
-          OR: [
-            {
-              loyverseItemID: input.id,
-            },
-            {
-              loyverseVariantID: input.id,
-            },
-          ],
+          loyverseVariantID: input.id,
         },
       });
+
+      const { data: loyverseVariant }: { data: Variant; } = await axios.get(API_BASE + "/variants/" + input.id, {
+        headers: {
+          'Authorization': AUTH
+        }
+      });
+
+      const { data: loyverseItem }: { data: Item; } = await axios.get(API_BASE + "/items/" + input.id, {
+        headers: {
+          'Authorization': AUTH
+        }
+      });
+
+      let returnedItem = {
+        loyverseItemId: loyverseVariant.item_id,
+        loyverseVariantId: loyverseVariant.variant_id,
+        name: loyverseItem.item_name,
+
+      }
+      
+
+
     }),
 
   updateDatabase: publicProcedure.mutation(async ({ ctx }) => {
