83cfd529aa7dad1cd7695798ba65373bb7501ad2
shringo <143767857+shringo@users.noreply.github.com>
2024-04-29 14:36:01 -1000

    make updateDatabase work

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -7,12 +7,16 @@ import { api } from "~/utils/api";
 
 export default function product() {
 
-    const x = api.mongo.getItemByLoyverseVariantId.useQuery({ id: "xxxx" });
+    const x = api.mongo.updateDatabase.useMutation();
+
+    x.mutate();
+    
+    //api.mongo.getItemByLoyverseVariantId.useQuery({ id: "xxxx" });
     return (
         <>
         <Navbar />
             <div className="pt-24 bg-cream1 h-screen">
-                {JSON.stringify(x.data, null, '\t')}
+                {x.isLoading ? "fuhhuhjifbdcrgdbfbdcvenhnbhhgh" : JSON.stringify(x.data, null, '\t')}
             </div>
             <Footer />
         </>
